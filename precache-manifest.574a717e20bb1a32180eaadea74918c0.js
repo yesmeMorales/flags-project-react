a0e28aece190fbb6bcd9efcb74412bad1f7934f3
Yesme <moralesyesme@gmail.com>
2020-06-26 17:08:39 -0500

    Updates

---
+++ precache-manifest.574a717e20bb1a32180eaadea74918c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b07c605ced8ee5422d1ebc78fc1f6f6e",
+    "url": "/flags-project-react/index.html"
+  },
+  {
+    "revision": "243e29a8ecbe7108f8c3",
+    "url": "/flags-project-react/static/css/main.20c5646e.chunk.css"
+  },
+  {
+    "revision": "eca5156cb6e9d3fe9b38",
+    "url": "/flags-project-react/static/js/2.10547045.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/flags-project-react/static/js/2.10547045.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "243e29a8ecbe7108f8c3",
+    "url": "/flags-project-react/static/js/main.b5e72e17.chunk.js"
+  },
+  {
+    "revision": "458cc2d46000523654f4",
+    "url": "/flags-project-react/static/js/runtime-main.770283a2.js"
+  }
+]);
